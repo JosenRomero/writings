81f7b33ebcfcbcb0f94d6058501c43fe5359d133
Jose <romerojosemanuel@hotmail.com>
2024-07-03 17:43:01 -0600

    add a background

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@
 const Layout = ({ children }) => {
   return (
     <div className="min-h-screen grid grid-cols-1 place-content-between font-serif">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
       <nav></nav>
       <main className="mx-auto">
         {children}
