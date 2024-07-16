1af8d5561db054fb7f8fbad0d69b064cc7c8abb5
Jose <romerojosemanuel@hotmail.com>
2024-07-16 16:25:51 -0600

    remove background

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,8 +4,7 @@ import Menu from "./Menu"
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen grid grid-cols-1 font-serif">
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+    <div className="min-h-screen grid grid-cols-1 font-serif bg-white">
       <Menu />
       <main className="p-6 w-full md:w-3/5 mx-auto row-span-4">
         <ToastContainer />
